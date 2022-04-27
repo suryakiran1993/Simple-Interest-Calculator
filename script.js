ad5d291c3c8ec9a92e47df61ac0945cc2bba8680
Jonnalagadda Surya Kiran <suryakiran1993@users.noreply.github.com>
2022-04-27 17:49:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
     
+    output = (p*r*y)/100
+    
+    document.getElementById("result").value = output
 }
         
