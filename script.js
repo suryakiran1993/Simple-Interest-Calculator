d22abed72a95406e387ba5ede8a957cb795b6c0e
Jonnalagadda Surya Kiran <suryakiran1993@users.noreply.github.com>
2022-04-27 17:52:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    
-    output = (p*r*y)/100
-    
-    document.getElementById("result").value = output
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    //validation principal
+    if(principal <= 0) {
+        alert("Enter a positive number");
+        myFocus();
+    } else {
+        document.getElementById("result").innerHTML = "If you deposit \<span style='background-color:yellow;'\>" + principal + "\<\/span\>,\<br\>at an interest rate of \<span style='background-color:yellow;'\>" + rate + "%\<\/span\>.\<br\>You will receive an amount of \<span style='background-color:yellow;'\>" + interest + "\<\/span\>,\<br\>in the year \<span style='background-color:yellow;'\>" + year + "\<\/span\>\<br\>";
+    }
 }
+
+//onfocus principal
+function myFocus() {
+    document.getElementById("principal").focus();
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+
         
